68a9e96910bbae82e7ee48786a1320eef593d5d0
doctorai-in <54732569+doctorai-in@users.noreply.github.com>
2020-09-23 14:11:17 +0530

    Update kubeflow-installation.sh

--- kubeflow-installation.sh
+++ kubeflow-installation.sh
@@ -79,8 +79,8 @@ export PATH=$PATH:${kubeflow}
 export KFAPP=kfapp
 export CONFIG=${kubeflow}/kfctl_k8s_istio.0.6.2.yaml
 
-kfctl init ${KFAPP} --config=${CONFIG} -V
+/home/kubeflow/kfctl init ${KFAPP} --config=${CONFIG} -V
 
 cd ${KFAPP}
-kfctl generate all -V        
-kfctl apply all -V
+/home/kubeflow/kfctl generate all -V        
+/home/kubeflow/kfctl apply all -V
