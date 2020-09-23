fdec54f19ca6982b0fa4fc46dc53ee33d38228dd
doctorai-in <54732569+doctorai-in@users.noreply.github.com>
2020-09-23 13:53:28 +0530

    Update kubeflow-installation.sh
    docker install command update

--- kubeflow-installation.sh
+++ kubeflow-installation.sh
@@ -1,15 +1,21 @@
 # First Time Installation.
 echo "First Time Installation "
 # Install Docker Community Edition.
-apt-get update
-apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository \
-    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-    $(lsb_release -cs) \
-    Stable"
-apt-get update
-apt-get install -y docker-ce
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo docker run hello-world
 
 # Install kubectl 
 sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.15.0/bin/linux/amd64/kubectl
